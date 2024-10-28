392924dfed60e2d6f0f8474166fffbf33d371395
Samat Kalshabekov <fatdrfrog@gmail.com>
2024-10-28 23:11:08 +0500

    fix: Vercel badge unfinished

---
+++ app/Band.tsx
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
+import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
+
+const Band = ()=> {
+    // References for the band and the joints
+    const band = useRef()
+    const fixed = useRef()
+    const j1 = useRef()
+    const j2 = useRef()
+    const j3 = useRef()
+    // Canvas size
+    const { width, height } = useThree((state) => state.size)
+    // A Catmull-Rom curve
+    const [curve] = useState(() => new THREE.CatmullRomCurve3([
+      new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()
+    ]))
+  
+  
+    useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1])
+    useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1])
+    useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1])
+  
+  }
+
+
+  export default Band;
